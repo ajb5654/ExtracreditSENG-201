39595f570da839cc42e79c7de35c798c5846b462
Aidan Bowen <Aidanbowen17@gmail.com>
2024-11-17 20:49:38 -0500

    Extra_Credit function

--- math_ops.py
+++ math_ops.py
@@ -4,8 +4,12 @@ def divide_numbers(a, b):
     """Divides two numbers and returns the result."""
     return a / b  # Potential division by zero error
 
+def extra_credit():
+    print('I want extra credit like jesus wants love, I want extra credit like how the thirsty being wants water, I want extra credit because I bombed the first quiz and this class is my Only B')
+
 if __name__ == "__main__":
     x = 10
     y = 0
     result = divide_numbers(x, y)
     print(f"The result of division is: {result}")
+    extra_credit()
